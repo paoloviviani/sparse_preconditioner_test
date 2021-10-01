6c1236d16c5c4de096a4c1308a29b782365d0e40
Paolo Viviani <paolo.viviani@linksfoundation.com>
2021-10-01 12:33:50 +0200

    sync

---
+++ ginkgo/ginkgo_precond.cpp
@@ -0,0 +1,13 @@
+#include <ginkgo/ginkgo.hpp>
+
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+
+int main()
+{
+	std::cout << "Hello, world!\n";
+}
