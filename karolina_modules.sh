58e19d8f3a4f25304a5a8db219d1ecd98e938dbb
viv0009 <viv0009@login1.barbora.it4i.cz>
2021-10-19 16:42:15 +0200

    wip

--- karolina_modules.sh
+++ karolina_modules.sh
@@ -5,5 +5,9 @@ module load numactl/2.0.13-GCCcore-10.2.0
 module load CMake
 module load hwloc/2.2.0-GCCcore-10.2.0
 module load OpenBLAS/0.3.12-GCC-10.2.0
+module load Boost/1.74.0-GCC-10.2.0
+module load OpenMPI/4.1.1-GCC-10.2.0
+module load netCDF/4.7.4-gompi-2020b
+module load GMP/6.2.0-GCCcore-10.2.0
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/viv0009/opt/ginkgo/lib64:/home/viv0009/opt/magma/lib
