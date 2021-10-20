8209328295d1a674bc39e0336e66609437fd1d63
viv0009 <viv0009@login2.barbora.it4i.cz>
2021-10-20 11:15:14 +0200

    wip

--- opm_modules.sh
+++ opm_modules.sh
@@ -9,5 +9,6 @@ module load Boost/1.74.0-GCC-10.2.0
 module load OpenMPI/4.1.1-GCC-10.2.0
 module load netCDF/4.7.4-gompi-2020b
 module load GMP/6.2.0-GCCcore-10.2.0
+module load MPFR/4.1.0-GCCcore-10.2.0
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/viv0009/opt/ginkgo/lib64:/home/viv0009/opt/magma/lib
