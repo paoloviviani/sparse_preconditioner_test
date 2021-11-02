bac2d92d792c491b90e752eb66f453b4485af2e5
viv0009 <viv0009@login1.barbora.it4i.cz>
2021-11-02 13:23:42 +0100

    sync

---
+++ ginkgo/ginkgo_solver.cpp
@@ -0,0 +1,153 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+
+#include <ginkgo/ginkgo.hpp>
+
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <chrono>
+
+int main(int argc, char *argv[])
+{
+    // Some shortcuts
+    using ValueType = double;
+    using RealValueType = gko::remove_complex<ValueType>;
+    using IndexType = int;
+
+    using vec = gko::matrix::Dense<ValueType>;
+    using real_vec = gko::matrix::Dense<RealValueType>;
+    using mtx = gko::matrix::Csr<ValueType, IndexType>;
+    using solver_type = gko::solver::Bicgstab<ValueType>;
+
+
+    if (argc == 2 && (std::string(argv[1]) == "--help")) {
+        std::cerr << "Usage: " << argv[0] << " [executor]" << std::endl;
+        std::exit(-1);
+    }
+
+    const auto executor_string = argc >= 2 ? argv[1] : "reference";
+    // Figure out where to run the code
+    std::map<std::string, std::function<std::shared_ptr<gko::Executor>()>>
+        exec_map{
+            {"omp", [] { return gko::OmpExecutor::create(); }},
+            {"cuda",
+             [] {
+                 return gko::CudaExecutor::create(0, gko::OmpExecutor::create(),
+                                                  true);
+             }},
+            {"hip",
+             [] {
+                 return gko::HipExecutor::create(0, gko::OmpExecutor::create(),
+                                                 true);
+             }},
+            {"dpcpp",
+             [] {
+                 return gko::DpcppExecutor::create(0,
+                                                   gko::OmpExecutor::create());
+             }},
+            {"reference", [] { return gko::ReferenceExecutor::create(); }}};
+
+    // executor where Ginkgo will perform the computation
+    const auto exec = exec_map.at(executor_string)();  // throws if not valid
+
+    // Read data
+    auto A = gko::share(gko::read<mtx>(std::ifstream("../data/prob_0_time_000000000000__nit_0_matrix_istl.mm"), exec));
+    auto b = gko::read<vec>(std::ifstream("../data/prob_0_time_000000000000__nit_0_rhs_istl.mm"), exec);
+
+    gko::size_type num_rows = A->get_size()[0];
+    auto host_x = vec::create(exec->get_master(), gko::dim<2>(num_rows, 1));
+    for (gko::size_type i = 0; i < num_rows; i++) {
+        host_x->at(i, 0) = 1.;
+    }
+    auto x = gko::clone(exec, host_x);
+
+    // Generate incomplete factors using ParILU
+    auto par_ilu_fact =
+        gko::factorization::ParIlu<ValueType, IndexType>::build().on(exec);
+    // Generate concrete factorization for input matrix
+    auto par_ilu = par_ilu_fact->generate(A);
+
+    // Generate an ILU preconditioner factory by setting lower and upper
+    // triangular solver - in this case the exact triangular solves
+    auto ilu_pre_factory =
+        gko::preconditioner::Ilu<gko::solver::LowerTrs<ValueType, IndexType>,
+                                 gko::solver::UpperTrs<ValueType, IndexType>,
+                                 false>::build()
+            .on(exec);
+
+    // Use incomplete factors to generate ILU preconditioner
+    auto t_start = std::chrono::high_resolution_clock::now();
+    
+    auto ilu_preconditioner = ilu_pre_factory->generate(gko::share(par_ilu));
+
+    // Use preconditioner inside GMRES solver factory
+    // Generating a solver factory tied to a specific preconditioner makes sense
+    // if there are several very similar systems to solve, and the same
+    // solver+preconditioner combination is expected to be effective.
+    const RealValueType reduction_factor{1e-2}; // 10e-2 to match OM-flow strategy, originally 10e-7
+    auto solver_factory =
+        solver_type::build()
+            .with_criteria(
+                gko::stop::Iteration::build().with_max_iters(1000u).on(exec),
+                gko::stop::ResidualNorm<ValueType>::build()
+                    .with_reduction_factor(reduction_factor)
+                    .on(exec))
+            .with_generated_preconditioner(gko::share(ilu_preconditioner))
+            .on(exec);
+
+    // Generate preconditioned solver for a specific target system
+    auto solver = solver_factory->generate(A);
+
+    // Solve system
+    solver->apply(gko::lend(b), gko::lend(x));
+    auto t_end = std::chrono::high_resolution_clock::now();
+    std::cout << "Time: " << std::chrono::duration<double>(t_end-t_start).count() << std::endl;
+
+    // Print solution
+    // std::cout << "Solution (x):\n";
+    // write(std::cout, gko::lend(x));
+
+    // Calculate residual
+    auto one = gko::initialize<vec>({1.0}, exec);
+    auto neg_one = gko::initialize<vec>({-1.0}, exec);
+    auto res = gko::initialize<real_vec>({0.0}, exec);
+    A->apply(gko::lend(one), gko::lend(x), gko::lend(neg_one), gko::lend(b));
+    b->compute_norm2(gko::lend(res));
+
+    std::cout << "Residual norm sqrt(r^T r):\n";
+    write(std::cout, gko::lend(res));
+}
