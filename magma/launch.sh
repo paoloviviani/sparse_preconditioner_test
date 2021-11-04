2b426efc16671eb9d5346e51fcba6e4b4f5d2be6
viv0009 <viv0009@login1.barbora.it4i.cz>
2021-11-04 12:55:57 +0100

    sync

--- magma/launch.sh
+++ magma/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # ./magma_precond --precond ILU  ../data/prob_0_time_000000000000__nit_0_matrix_istl.mm
-./magma_solver --precond ILU --solver BICGSTAB --rtol 10e-2 --maxiter 1000 ../data/prob_0_time_000000000000__nit_0_matrix_istl.mm ../data/prob_0_time_000000000000__nit_0_rhs_istl_magma.mm 
+./magma_dsolver --precond ILU --solver BICGSTAB --rtol 1e-2 --maxiter 1000 ../data/prob_0_time_000000000000__nit_0_matrix_istl.mm ../data/prob_0_time_000000000000__nit_0_rhs_istl_magma.mm 
